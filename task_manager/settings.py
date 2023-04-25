1eeeb01e300dcc669a547ed6db0b7112d53ba373
LeonidBabkin <babkinleo@gmail.com>
2023-04-25 16:56:43 +0300

    settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cmk4i)=n!d0($0wdn2hawwjheac%17hg__774+=k3p7m20_7_b'
-
 # SECURITY WARNING: don't run with debug turned on in production
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', False)
