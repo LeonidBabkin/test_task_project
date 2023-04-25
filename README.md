e102599e0982e1ee56cfe6b170cba8ee9fc299f9
LeonidBabkin <babkinleo@gmail.com>
2023-04-25 19:54:45 +0300

    changing to postgresql

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/LeonidBabkin/python-project-52/workflows/hexlet-check/badge.svg)](https://github.com/LeonidBabkin/python-project-52/actions)
+
+link: https://task-manager-app-kzsy.onrender.com
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -83,7 +83,7 @@ DB_PORT = parsed_url.port
 DB_NAME = parsed_url.path[1:]
 DB_USER = parsed_url.username
 DB_PASS = parsed_url.password
-print('DB_HOST:', DB_HOST, 'DB_PORT:', DB_PORT, 'DB_NAME:', DB_NAME, 'DB_USER:', DB_USER, 'DB_PASS:', DB_PASS)
+# print('DB_HOST:', DB_HOST, 'DB_PORT:', DB_PORT, 'DB_NAME:', DB_NAME, 'DB_USER:', DB_USER, 'DB_PASS:', DB_PASS)
 
 DATABASES = {
     'default': {
