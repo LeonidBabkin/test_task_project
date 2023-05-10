b64ae9ecbdc1fec61d988c1099883155e8829ccf
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-05-10 15:58:10 +0300

    Update views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -36,3 +36,23 @@ class TaskCreateView(CreateView):
             return redirect(reverse_lazy('home'))
         else:
             return render(request, 'tasks/create_task.html', {'form': form})
+# class TaskCreateView(CreateView):
+
+#     def get(self, request, *args, **kwargs):
+#         form = CreateTaskForm()
+#         return render(request, 'tasks/create_task.html', {'form': form})
+
+#     def post(self, request, *args, **kwargs):
+#         form = CreateTaskForm(request.POST)
+#         if form.is_valid():
+# # That's useful when you get most of your model data from a form, but you need to populate
+# # some null=False fields with non-form data.Saving with commit=False gets you a model object,
+# # then you can add your extra data and save it.
+#             post = form.save(commit=False)
+#             # fill in the field author with with current user id
+#             post.author = NewUser.objects.get(id=request.user.id)
+#             post.save()
+#             messages.info(request, tr('Задача успешно создана'))
+#             return redirect('tasks')
+#         else:
+#             return render(request, 'tasks/create_task.html', {'form': form})
